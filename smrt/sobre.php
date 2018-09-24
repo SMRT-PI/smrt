17282ca0fb2049632cf1a9c9f1eedcef6c7d3348
LaraBez <>
2018-09-24 17:19:52 -0300

    Estilização do site e perfil

---
+++ smrt/sobre.php
@@ -0,0 +1,16 @@
+
+<html>
+    
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        require_once 'cabecalho.php';
+        require_once 'rodape.php';
+
+        ?>
+    </body>
+</html>
