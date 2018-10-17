76c697604507142ee33d1b02d79ecdf76bac1539
Lara Bez Fontana <Lara Bez Fontana@DESKTOP-1150KTJ.local>
2018-10-16 22:45:42 -0300

    Geolocalização PRONTA, ATÉ QUE ENFIM

--- smrt/publicacao/form_inserir.php
+++ smrt/publicacao/form_inserir.php
@@ -9,15 +9,12 @@ $linha = mysqli_fetch_array($resultado);
 
 ?>
 
-
-     
-<script async defer
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC7xO0yzbGdqeO7caYCe1PDDwzMw6TphtU&callback=initMap">
-</script>
 <script> 
     
     
-    
+var lat = '';
+var lon = ''; 
+ 
 var x=document.getElementById("demo");
 function getLocation()
   {
@@ -27,27 +24,7 @@ function getLocation()
     }
   else{x.innerHTML="Geolocalização não é suportada nesse browser."}
   }
- 
-function showPosition(position)
-  {
-  lat=position.coords.latitude;
-  lon=position.coords.longitude;
-  latlon=new google.maps.LatLng(lat, lon)
-  mapholder=document.getElementById('mapholder')
-  mapholder.style.height='250px';
-  mapholder.style.width='500px';
-
-
-  var myOptions={
-  center:latlon,zoom:14,
-  mapTypeId:google.maps.MapTypeId.ROADMAP,
-  mapTypeControl:false,
-  navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
-  };
-  var map=new google.maps.Map(document.getElementById("mapholder"),myOptions);
-  var marker=new google.maps.Marker({position:latlon,map:map,title:"Minha localização!"});
-  }
- 
+  
 function showError(error)
   {
   switch(error.code) 
@@ -67,18 +44,48 @@ function showError(error)
     }
   }
 
+function showPosition(position)
+  {
+  lat=position.coords.latitude;
+  lon=position.coords.longitude;
+
+  mapholder=document.getElementById('mapholder');
+  mapholder.style.height='250px';
+  mapholder.style.width='500px';
+  mapholder.innerHTML = ('<iframe id="mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d876.7773817165236!2d' + lon + '!3d' + lat + '!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjjCsDI4JzM0LjUiUyA0OcKwMDEnMzQuNiJX!5e0!3m2!1spt-BR!2sbr!4v1539659561200" width="600" height="450" frameborder="0" style="border:0;width:100%;height:100%;" allowfullscreen></iframe>');
+
+  document.getElementById("lat").value = lat;
+  document.getElementById("lon").value = lon;
+
+  /*
+  var latlon=new google.maps.LatLng(lat, lon)
+  var myOptions={
+  center:latlon,zoom:14,
+  mapTypeId:google.maps.MapTypeId.ROADMAP,
+  mapTypeControl:false,
+  navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
+  };
+  var map=new google.maps.Map(document.getElementById("mapholder"),myOptions);
+  var marker=new google.maps.Marker({position:latlon,map:map,title:"Minha localização!"});
+  */
+  }
 
 //CHAVE API GOOGLE MAPS - AIzaSyC7xO0yzbGdqeO7caYCe1PDDwzMw6TphtU
 
 </script>
-                
-            <script type="text/javascript">
-               var lat = 'teste';
-               var lon = 'teste'; 
-            </script>
+
+
 
 <div class="container">
    
+<!--    Mapa-->
+<!--O mapa ta em um ponto fixo. Como fazer com que ele pegue a localização do próprio usuario? 
+Por variarél ali no lugar das coordenadas?
+Pelo menos o mapa ta funcionando agora hsahsuahu -->
+
+    
+    
+<!--    Mapa-->    
     <div class="col-md-6 offset-md-3 mt-5 mb-5">  
         <div class="row mt-3 mb-3">
                 <div id="mapholder" style="margin:0 auto;"></div>
@@ -86,15 +93,8 @@ function showError(error)
         <div class="row">
             <div class="col-md-12 mb-4">
                 <button class="btn btn-primary float-right" onclick="getLocation()">Obtenha a localização aqui!</button>
+                <p id="demo"></p>
             </div>
-            
-            <?php 
-                $lat = "<script>document.write(lat)</script>";
-                echo "$lat"; 
-                $lon = "<script>document.write(lon)</script>";
-                echo "$lon";
-            ?>
-
         </div>
         <form action="action" method="post" action="inserir.php">
           <input type="hidden" name="autor" value="<?= $linha['id'] ?>"/>   
@@ -102,8 +102,8 @@ function showError(error)
           <div class="form-group row">
             <label for="inputEmail3" class="col-sm-2 col-form-label">Coordenadas:</label>
             <div class="col-sm-10">
-                <input class="form-control" type="text" name="titulo" value="<?= $lat?>" placeholder="Latitude"/>
-                <input class="form-control" type="text" name="titulo" value="<?= $lon?>" placeholder="Longitude"/>
+                <input class="form-control" type="text" id="lat" name="lat" value="" placeholder="Latitude"/>
+                <input class="form-control" type="text" id="lon" name="lon" value="" placeholder="Longitude"/>
             </div>
           </div>
           
