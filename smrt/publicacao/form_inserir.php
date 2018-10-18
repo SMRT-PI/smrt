4325767034d87d43147adec13bff11a05ff7c31b
Ezequias <Ezequias@DESKTOP-U1144N6>
2018-10-18 00:53:28 -0300

    Inserção da data na Publicação

--- smrt/publicacao/form_inserir.php
+++ smrt/publicacao/form_inserir.php
@@ -112,13 +112,13 @@ $Name_autor = $_SESSION['nome'];
             <div class="form-group row">
                 <label for="inputEmail3" class="col-sm-2 col-form-label">Título:</label>
                 <div class="col-sm-10">
-                    <input class="form-control" type="text" name="titulo" placeholder="Informe o título de publicação"/>
+                    <input class="form-control" type="text" name="legenda" placeholder="Informe o título de publicação"/>
                 </div>
             </div>
             <div class="form-group row">
                 <label for="inputEmail3" class="col-sm-2 col-form-label">Legenda:</label>
                 <div class="col-sm-10">
-                    <textarea class="form-control" rows="5" placeholder="Informe a legenda de Publicação" name="legenda"></textarea>
+                    <textarea class="form-control" rows="5" placeholder="Informe a legenda de Publicação" name="titulo"></textarea>
                 </div>
             </div>
 
--- smrt/publicacao/inserir.php
+++ smrt/publicacao/inserir.php
@@ -5,11 +5,12 @@ $legenda = $_POST['legenda'];
 $imagem = $_POST['imagem'];
 $autor = $Name_autor;
 $titulo = $_POST['titulo'];
+$data = (idate("Y") . "-" . idate("m") . "-" . idate("d"));
 
 
 include '../bd/conectar.php';
 
-$sql_pessoa = "INSERT INTO pub (legenda, imagem, autor, titulo) VALUES ('$legenda', '$imagem','$autor','$titulo')";
+$sql_pessoa = "INSERT INTO pub (legenda, imagem, autor, titulo, dataa) VALUES ('$legenda', '$imagem','$autor','$titulo','$data')";
 
 mysqli_query($conexao, $sql_pessoa);
 //echo $autor;
