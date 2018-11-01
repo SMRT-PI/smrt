027dc4fa3bec60320a7d756f26647f0a208b0a17
Luca <Luca@DESKTOP-0DCNTDR>
2018-11-01 13:24:05 -0200

    ARRUMANDO ESTILO DA VALIDAÇÃO

--- smrt/usuario/cadastrar.php
+++ smrt/usuario/cadastrar.php
@@ -1,45 +1,9 @@
 <?php
 
-//sleep(1);
-//include_once '../bd/conectar.php';
-//if (isset($_POST['cadastrar']) && $_POST['cadastrar'] == 'sim'):
-//    $novos_campos = array();
-//    $campos_post = $_POST['campos'];
-//
-//    $respostas = array();
-//    foreach ($campos_post as $indice => $valor) {
-//        $novos_campos[$valor['name']] = $valor['value'];
-//    }
-//
-//    if (!strstr($novos_campos['email'], '@')) {
-//        $respostas['erro'] = 'sim';
-//        $respostas['getErro'] = 'E-mail Inválido, Preencha com E-mail Válido';
-//    } elseif ($novos_campos['senha'] != $novos_campos['csenha']) {
-//        $respostas['erro'] = 'sim';
-//        $respostas['getErro'] = 'As Senhas não Correspondem!';
-//    } elseif (strlen($novos_campos['nome']) < 2) {
-//        $respostas['erro'] = 'sim';
-//        $respostas['getErro'] = 'Preencha com um Nome Válido';
-//    } else {
-//        $respostas['erro'] = 'nao';
-//        $respostas['msg'] = 'Cadastrado com Sucesso!';
-//    }
-//
-//
-//    echo json_encode($respostas);
-//
-//
-//endif;
-?>
-
-<?php
-
-//action.php
-
 sleep(2);
 
 if (isset($_POST['nome'])) {
-    $connect = new PDO("mysql:host=localhost;dbname=smrt", "root", "ifsc");
+    $connect = new PDO("mysql:host=localhost;dbname=smrt", "root", "");
 
     $data = array(
         ':nome' => $_POST['nome'],
--- smrt/usuario/entrar.php
+++ smrt/usuario/entrar.php
@@ -9,13 +9,13 @@ include '../cabecalho.php';
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script src="http://parsleyjs.org/dist/parsley.js"></script> <!-- Biblioteca de validação de formulários JavaScript -->
 
-<div id="form_cadastrar" class="container-fluid mb-4">
+<div id="form_cadastrar" class="container mb-4">
     <div class="container-fluid text-center">
         <div class="row justify-content-center">
             <form id="logar" class="col-lg-6" method="post" action="logar.php" name="logar"><br>
-                <div class="jumbotron jumbotron-fluid text-center m-0 h2" style="background-color: whitesmoke;">Entre com sua Conta!</div>
-                <input class="form-control py-3" id="email_login" type="email" name="email" placeholder="E-mail">
-                <input class="form-control py-3 mt-3 mb-4" id="senha_login" type="password" name="senha" placeholder="Senha">
+                <div class="jumbotron jumbotron-fluid text-center m-0 h2 py-4" style="background-color: whitesmoke;">Entre com sua Conta!</div>
+                <input class="form-control py-2" id="email_login" type="email" required name="email" placeholder="E-mail">
+                <input class="form-control py-2 mt-3 mb-4" id="senha_login" type="password" name="senha" placeholder="Senha">
                 <div class="btn-group w-100 justify-content-center">
                     <a class="btn btn-default mr-2 rounded" href="#first_name">Não é Cadastrado?</a>
                     <input class="btn btn-success rounded" type="submit" name="logar" value="ENTRAR">
@@ -23,12 +23,12 @@ include '../cabecalho.php';
             </form>
 
             <form id="validate_form" class="col-lg-6"><br>
-                <div class="jumbotron jumbotron-fluid text-center m-0 h2" style="background-color: whitesmoke;">Cadastre-se Gratuitamente!</div>
-                <input class="form-control py-3" type="text" name="nome" id="first_name" placeholder="Nome" required data-parsley-pattern="^[a-zA-Z]+$" data-parsley-trigger="keyup">
-                <input class="form-control py-3 my-3" type="text" name="sobrenome" id="last_name" placeholder="Sobrenome" required data-parsley-pattern="^[a-zA-Z ]+$" data-parsley-trigger="keyup">
-                <input class="form-control py-3" type="text" name="email" id="email" placeholder="Email" required data-parsley-type="email" data-parsley-trigger="keyup">
-                <input class="form-control py-3 my-3" type="password" name="senha" id="password" placeholder="Senha" required data-parsley-length="[8, 16]" data-parsley-trigger="keyup">
-                <input class="form-control py-3 mb-3" type="password" name="confirm_senha" id="confirm_password" placeholder="Confirme a senha"data-parsley-equalto="#password" data-parsley-trigger="keyup" required>
+                <div class="jumbotron jumbotron-fluid text-center m-0 h2 py-4" style="background-color: whitesmoke;">Cadastre-se Gratuitamente!</div>
+                <input class="form-control py-2" type="text" name="nome" id="first_name" placeholder="Nome" required data-parsley-pattern="^[a-zA-Z]+$" data-parsley-trigger="keyup">
+                <input class="form-control py-2 my-3" type="text" name="sobrenome" id="last_name" placeholder="Sobrenome" required data-parsley-pattern="^[a-zA-Z ]+$" data-parsley-trigger="keyup">
+                <input class="form-control py-2" type="text" name="email" id="email" placeholder="Email" required data-parsley-type="email" data-parsley-trigger="keyup">
+                <input class="form-control py-2 my-3" type="password" name="senha" id="password" placeholder="Senha" required data-parsley-length="[8, 16]" data-parsley-trigger="keyup">
+                <input class="form-control py-2 mb-3" type="password" name="confirm_senha" id="confirm_password" placeholder="Confirme a senha"data-parsley-equalto="#password" data-parsley-trigger="keyup" required>
                 <div class="btn-group w-100 justify-content-center">
                     <a class="btn btn-default mr-2 rounded" href="#email_login">Já é Cadastrado?</a>
                     <input class="next acao btn btn-success rounded" type="submit" id="submit" name="submit" value="CADASTRAR" >
